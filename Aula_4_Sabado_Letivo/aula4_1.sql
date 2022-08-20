d8bd90225d482046975739ae8a4b890d6f66ae20
Brunno de Souza Oliveira <110069192+BrunnoSouzaOliveira@users.noreply.github.com>
2022-08-20 11:33:47 -0300

    Aula 4

---
+++ Aula_4_Sabado_Letivo/aula4_1.sql
@@ -0,0 +1,12 @@
+SELECT 
+    *
+FROM
+    city
+WHERE city.CountryCode = (
+    SELECT
+        Code
+    FROM
+        country
+    WHERE
+        country.Name = "Afghanistan"
+);
--- aula3.sql
+++ aula3.sql
@@ -1,9 +1,3 @@
-/*Quais filmes Kim assistiu?
-customer
-rental
-inventory
-film*/
-
 USE sakila;
 
 SET @ID_CUSTOMER_SELECTED = (SELECT
@@ -15,23 +9,23 @@ WHERE
 
 SELECT @ID_CUSTOMER_SELECTED as 'ID_CUSTOMER_SELECETED';
 
-SELECT 
+SELECT
     rental.inventory_id
-FROM 
+FROM
     rental
 WHERE
     rental.customer_id = @ID_CUSTOMER_SELECTED;
 
-SELECT 
+SELECT
     inventory.film_id
-FROM 
+FROM
     inventory
 WHERE
     inventory.inventory_id IN (
         (
-            SELECT 
+            SELECT
                 rental.inventory_id
-            FROM 
+            FROM
                 rental
             WHERE
                 rental.customer_id = @ID_CUSTOMER_SELECTED
@@ -40,7 +34,7 @@ WHERE
 
 SELECT
     "What Actors was in those Movies"
-AS  
+AS
     "INFO";
 
 SELECT
@@ -49,150 +43,33 @@ SELECT
     actor.last_name
 FROM
     actor
-WHERE actor.actor_id IN ( 
-    SELECT 
-        film_actor.film_id 
-    FROM 
+WHERE
+actor.actor_id IN ( 
+    SELECT
+        film_actor.actor_id
+    FROM
         film_actor 
-    WHERE 
-        film_actor.film_id IN (
-            SELECT
-                film.film_id
-            FROM 
-                film 
-            WHERE 
-                film.film_id IN (
-                SELECT 
-                    inventory.film_id
-                FROM 
-                    inventory
-                WHERE
-                    inventory.inventory_id IN (
-                        SELECT 
-                            rental.inventory_id
-                        FROM 
-                            rental
-                        WHERE
-                            rental.customer_id = (
-                                SELECT
-                                    customer.customer_id
-                                FROM
-                                    customer
-                                WHERE
-                                    customer.email = 'KIM.CRUZ@sakilacustomer.org'
-                            )
-                    )  
-            )
-        )
-);
-
-SELECT
-    "Which was the id of the film_actor.film_id"
-AS  
-    "INFO";
-
-SELECT
-    *
-FROM
-    film_actor
-WHERE
-    film_actor.actor_id = "11" OR film_actor.actor_id = "115";
-
-SELECT
-    "Name of the Movies"
-AS  
-    "INFO";
-
-SELECT
-    film.film_id,
-    film.title,
-    film.description
-FROM
-    film
-WHERE
-    film.film_id IN (
-        118,
-        205,
-        281,
-        283,
-        348,
-        364,
-        395,
-        429,
-        433,
-        453,
-        485,
-        532,
-        567,
-        587,
-        597,
-        636,
-        709,
-        850,
-        854,
-        888,
-        896,
-        928,
-        938,
-        969,
-        988,
-        49,
-        52,
-        245,
-        246,
-        277,
-        302,
-        379,
-        383,
-        391,
-        428,
-        506,
-        531,
-        607,
-        615,
-        661,
-        671,
-        686,
-        703,
-        714,
-        740,
-        754,
-        846,
-        887,
-        952,
-        955,
-        966,
-        985,
-        994
-    );
-
-/*SELECT 
-    film.film_id,
-    film.title,
-    film.description
-FROM 
-    film 
-WHERE 
-    film_id IN (
-        SELECT 
+    WHERE
+    film_actor.film_id IN (
+        SELECT
             inventory.film_id
-        FROM 
+        FROM
             inventory
         WHERE
-            inventory.inventory_id IN (
-                SELECT 
-                    rental.inventory_id
-                FROM 
-                    rental
+        inventory.inventory_id IN (
+            SELECT
+                rental.inventory_id
+            FROM
+                rental
+            WHERE
+            rental.customer_id = (
+                SELECT
+                    customer.customer_id
+                FROM
+                    customer
                 WHERE
-                    rental.customer_id = (
-                        SELECT
-                            customer.customer_id
-                        FROM
-                            customer
-                        WHERE
-                            customer.email = 'KIM.CRUZ@sakilacustomer.org'
-                    )
-            )   
+                    customer.email = 'KIM.CRUZ@sakilacustomer.org'
+            )
+        )
     )
-ORDER BY film_id ASC;*/
+);
