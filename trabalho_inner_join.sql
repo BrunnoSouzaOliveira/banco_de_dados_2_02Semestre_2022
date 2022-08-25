df9fe3c240607168eebaaa9a1b5f9ae06a540eea
Brunno de Souza Oliveira <110069192+BrunnoSouzaOliveira@users.noreply.github.com>
2022-08-25 20:57:29 -0300

    Trabalho para saber quais filmes os residentes de Caracas assistem

---
+++ trabalho_inner_join.sql
@@ -0,0 +1,52 @@
+USE sakila;
+
+SELECT
+    film.title,
+    film.description
+FROM
+    film
+WHERE film.film_id IN (
+    SELECT
+        inventory.inventory_id
+    FROM
+        inventory
+    WHERE inventory.film_id IN (
+        SELECT
+            rental.customer_id
+        FROM
+            rental
+        WHERE rental.inventory_id IN (
+            SELECT
+                 customer.address_id
+            FROM
+                customer
+            WHERE customer.customer_id IN (
+                SELECT
+                    address.city_id
+                FROM
+                    address
+                WHERE address.address_id = (
+                    SELECT
+                        city.city_id
+                    FROM
+                        city
+                    WHERE
+                        city.city = "Caracas"
+                )
+            )
+        )
+    )
+)
+    
+    
+    /*address ON city.city_id = address.city_id
+INNER JOIN
+    customer ON customer.address_id = address.address_id
+INNER JOIN
+    rental ON rental.customer_id = customer.customer_id
+INNER JOIN
+    inventory ON inventory.inventory_id = rental.inventory_id
+INNER JOIN
+    film ON film.film_id = inventory.film_id
+WHERE
+    city.city = "Caracas";*/
